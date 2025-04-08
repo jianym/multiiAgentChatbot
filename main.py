3e9045b9d009c70fbef04f5048eee2f33b7c31d5
jianym <3502168163@qq.com>
2025-04-08 08:55:58 +0800

    优化

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 
 from controller import chat
 from controller import knowledge
@@ -16,7 +17,10 @@ async def globalExceptionHandler(request: Request, exc: Exception):
     # 打印异常详情或记录日志
     print(f"Unexpected error: {exc}")
     # 返回自定义错误信息
-    return {"status": 500, "reply":""}
+    return JSONResponse(
+        status_code=200,
+        content={"code": 500, "reply": "当前执行错误，请重试"}
+    )
 
 
 # 中间件：模拟 AOP，统一拦截所有请求
@@ -31,7 +35,5 @@ class SessionValidationMiddleware(BaseHTTPMiddleware):
         # 在响应返回之前，你可以在这里做一些后置操作（例如日志记录）
         return response
 
-# 将中间件添加到应用中
+
 app.add_middleware(SessionValidationMiddleware)
-
-
