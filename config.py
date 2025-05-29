bf4045c9b83ef927bb40f6c68c75c627688bafe4
jianym <3502168163@qq.com>
2025-05-29 13:16:23 +0800

    优化

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ REDIS_URL = 'redis://127.0.0.1:6379/0'
 REDIS_BACKEND_URL = 'redis://127.0.0.1:6379/1'
 
 MAIL_HOST = "smtp.qq.com"
-MAIL_SNDDER = "110714284@qq.com"
+MAIL_SNDDER = "*********"
 MAIL_PORT = "587"
-MAIL_LICENSE = "salvxkzkffpkbijf"
+MAIL_LICENSE = "*********"
 
 DEEPSEEK_URL = "https://api.deepseek.com"
-DEEPSEEK_API_Key = "sk-b78ecff6a2864de9acf835d33d3aee52"
+DEEPSEEK_API_Key = "*********"
 
 QWEN_URL = "https://dashscope.aliyuncs.com/compatible-mode/v1"
-QWEN_API_Key = "sk-b69224f8f4444d0ebb014e4aa74fe54d"
+QWEN_API_Key = "*********"
 
-OS_BASE_PATH = "/Users/jianyiming/PycharmProjects/multiAgentChatbot"
+OS_BASE_PATH = "****************"
 
